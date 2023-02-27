ca94d38757776f6084b38ca49077486f5a8851b2
arvind sisodiya <57572062+arvindsis11@users.noreply.github.com>
2023-02-27 19:05:14 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,16 @@
+package com.arvind.fileprocessing.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	private String id;
+	private String name;
+	private String lastname;
+	private String email;
+
+}
