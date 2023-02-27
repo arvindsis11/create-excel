b90e24467867a213b287817bafe4df79cc20a4c6
arvind sisodiya <57572062+arvindsis11@users.noreply.github.com>
2023-02-27 19:04:02 +0530

    Create GenerateSheetTest.java

---
+++ GenerateSheetTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.arvind.fileprocessing.model.User;
+import com.arvind.fileprocessing.util.ExcelHelper;
+
+class GenerateList {
+
+	@Test
+	void test() {
+		List<User> obj = new ArrayList<>();
+		obj.add(new User("1", "john", "abc", "john@123"));
+		obj.add(new User("2", "kevinn", "cde", "kevinn@123"));
+		obj.add(new User("3", "alex", "fgh", "alex@123"));
+		obj.add(new User("4", "bob", "suv", "bob@123"));
+		ExcelHelper.UsersToExcel(obj);
+	}
+
+}
